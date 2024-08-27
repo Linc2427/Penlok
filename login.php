417bcb7a421f97a1fc0e0c0e75fdfb91b9b25e29
ferdykp <90691032+ferdykp@users.noreply.github.com>
2024-08-27 20:53:08 +0700

    login page

--- login.php
+++ login.php
@@ -30,53 +30,104 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="icon" type="image/ico" href="images/favicon.ico">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login_style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <title>Login 04</title>
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap"
+        rel="stylesheet" />
+
+    <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+    <link rel="stylesheet" href="dist/css/login.css" />
 </head>
 
 <body>
     <!-- <?php echo $_SESSION['error'] ?> -->
-    <div class="wrapper">
-        <div class="container main">
-            <div class="row">
-                <div class="col-md-6 side-image">
-                    <!-------Image-------->
-                </div>
-                <div class="col-md-6 right">
-                    <form action="" method="post" enctype="multipart/form">
-                        <div class="gambarap">
-                            <img src="assets/pens-panjang.png" alt="Logo Angkasa Pura">
-                        </div>
-                        <div class="input-box">
-                            <header>Login</header>
-                            <div class="input-field">
-                                <input type="text" class="input" name="user" id="user" required autocomplete="off" ?>
-                                <label for="user">User</label>
-                            </div>
-                            <div class="input-field">
-                                <input type="password" class="input" name="pass" id="pass" required>
-                                <label for="password">Password</label>
-                            </div>
-                            <div class="input-field">
-                                <input type="submit" name="submit" class="submit" value="Submit">
-                            </div>
-                        </div>
-                    </form>
+    <section style="background-image: url(assets/pens-gedung.jpg)" class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 text-center mb-5">
+                    <!-- <h2 class="heading-section">Login #04</h2> -->
                 </div>
             </div>
-        </div>
-    </div>
-</body>
-<footer class="d-flex flex-wrap justify-content-between align-items-center"></footer>
-                        <div class="col d-flex align-items-center">
-                            <div class="copyright fixed-bottom align-items-center mb-4">
-                                <p class="text  align-items-center">Copyright © 2023 Telkom-PENS. All rights reserved</p>
+            <form action="" method="post" enctype="multipart/form">
+                <div class="row justify-content-center">
+                    <div class="col-md-12 col-lg-10">
+                        <div class="wrap d-md-flex">
+                            <div
+                                class="img"
+                                style="background-image: url(assets/pens-gedung.jpg)"></div>
+                            <div class="login-wrap p-4 p-md-5">
+                                <div class="d-flex">
+                                    <div class="w-100">
+                                        <h5 class="mb-4">Welcome to Lab. Telephony</h5>
+                                        <h3 class="mb-4">Sign In</h3>
+                                    </div>
+                                </div>
+                                <form action="#" class="signin-form">
+                                    <div class="form-group mb-3">
+                                        <label class="label" for="name">Username</label>
+                                        <input
+                                            type="text"
+                                            name="user"
+                                            id="user"
+                                            class="form-control"
+                                            placeholder="Username"
+                                            required
+                                            autocomplete="off" />
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <label class="label" for="password">Password</label>
+                                        <input
+                                            type="password"
+                                            name="pass"
+                                            id="pass"
+                                            class="form-control"
+                                            placeholder="Password"
+                                            required />
+                                    </div>
+                                    <div class="form-group">
+                                        <button
+                                            type="submit"
+                                            name="submit"
+                                            value="submit"
+                                            class="form-control btn btn-primary rounded submit px-3">
+                                            Sign In
+                                        </button>
+                                    </div>
+                                    <div class="form-group d-md-flex">
+                                        <div class="w-50 text-left">
+                                            <label class="checkbox-wrap checkbox-primary mb-0">Remember Me
+                                                <input type="checkbox" checked />
+                                                <span class="checkmark"></span>
+                                            </label>
+                                        </div>
+                                        <div class="w-50 text-md-right">
+                                            <a href="#">Forgot Password</a>
+                                        </div>
+                                    </div>
+                                </form>
+                                <p class="text-center">
+                                    Not a member? <a data-toggle="tab" href="#signup">Sign Up</a>
+                                </p>
                             </div>
                         </div>
-</footer>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
 </html>
